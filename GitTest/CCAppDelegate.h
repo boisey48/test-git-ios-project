5b725d2fabbad1ca9e64ffdb112317642d5d6591
Erik Boisvert <erikboisvert@Eriks-MacBook-Pro.local>
2014-05-29 16:06:17 -0400

    Initial Commit

---
+++ GitTest/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  GitTest
+//
+//  Created by Erik Boisvert on 5/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/CCViewController.m
@@ -0,0 +1,29 @@
+//
+//  CCViewController.m
+//  GitTest
+//
+//  Created by Erik Boisvert on 5/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "CCViewController.h"
+
+@interface CCViewController ()
+
+@end
+
+@implementation CCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
