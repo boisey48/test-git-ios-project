272dcc15af9b6066f86130f64bb92b369731a14f
Erik Boisvert <erikboisvert@Eriks-MacBook-Pro.local>
2014-05-29 16:32:25 -0400

    added a test class

---
+++ CCtest.h
@@ -0,0 +1,13 @@
+//
+//  CCtest.h
+//  GitTest
+//
+//  Created by Erik Boisvert on 5/29/14.
+//  Copyright (c) 2014 Erik Boisvert. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCtest : NSObject
+
+@end
---
+++ CCtest.m
@@ -0,0 +1,13 @@
+//
+//  CCtest.m
+//  GitTest
+//
+//  Created by Erik Boisvert on 5/29/14.
+//  Copyright (c) 2014 Erik Boisvert. All rights reserved.
+//
+
+#import "CCtest.h"
+
+@implementation CCtest
+
+@end
